2a54bbc8e44df39c9eb0b9c10da78c7a11f3c394
CodeWithme-Ali <imalimubashir2121@gmail.com>
2025-07-30 15:04:26 +0500

    Area and Perimeter of a rectangle.
    This is a program which allows user to calculate the area and perimeter of a rectangle by entering appropriate values.

---
+++ area_perimeter of rectangle.py
@@ -0,0 +1,18 @@
+# Program to calculate area and perimeter of a rectangle.
+
+
+# Area of Rectangle...
+
+print("Please Enter Values Below to Calculate The Area of a Rectangle.")
+L = int(input("Enter Length of a rectangle in cm: "))
+W = int(input("Enter Width of a rectangle in cm: "))
+print(f"Area of a rectangle in cm is: {L*W}")
+
+
+# Perimeter of Rectangle...
+
+print("Please Enter Values Below to Calculate The Perimeter of a Rectangle.")
+L = int(input("Enter Length of a rectangle in cm: "))
+W = int(input("Enter Width of a rectangle in cm: "))
+print(f"Perimeter of a rectangle in cm is: {(L+W)*2}")
+print("Thank You")
