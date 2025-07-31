9e67575d762811bf5766499215d47d4f46cefef3
CodeWithme-Ali <imalimubashir2121@gmail.com>
2025-07-31 10:25:46 +0500

    Program to Add, subtract, multiply, and divide two numbers.
    This is a program which adds, subtracts, multiply and divide two numbers entered by users...

---
+++ add_sub_mul_div.py
@@ -0,0 +1,27 @@
+# Program to Add, subtract, multiply, and divide two numbers.
+
+print("Program to Add, Subtract, Multiply, and Divide two numbers...")
+print("Addition...")
+a = int(input("Enter Number 1 please: "))
+b = int(input("Enter Number 2 please: "))
+add = (a+b)
+print(f"Answer is: {add}")
+
+print("Subtraction...")
+a = int(input("Enter Number 1 please: "))
+b = int(input("Enter Number 2 please: "))
+subtract = (a-b)
+print(f"Answer is: {subtract}")
+
+print("Multiplication...")
+a = int(input("Enter Number 1 please: "))
+b = int(input("Enter Number 2 please: "))
+multiply = (a*b)
+print(f"Answer is: {multiply}")
+
+print("Division...")
+a = int(input("Enter Number 1 please: "))
+b = int(input("Enter Number 2 please: "))
+divide = (a/b)
+print(f"Answer is: {divide}")
+print("Thank You")
