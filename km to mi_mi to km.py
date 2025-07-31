5aee31f1c1e545ab622cd37409b668273f9faf3f
CodeWithme-Ali <imalimubashir2121@gmail.com>
2025-07-31 20:05:27 +0500

    program to Convert kilometers to miles and vice versa.
    program to Convert kilometers to miles and vice versa.

---
+++ km to mi_mi to km.py
@@ -0,0 +1,12 @@
+# program to Convert kilometers to miles and vice versa.
+
+print("\nConverting miles to kilometers...")
+a = int(input("Enter value of miles please: "))
+km = (a*1.6)
+print(f"Answer is {km} kilometers")
+
+print("\nConverting kilometers to miles...")
+b = int(input("Enter value of kilometers please: "))
+mi = (b/1.6)
+print(f"Answer is {mi} miles")
+print("\nThank You!")
